efdd5a58d36741462ed8e444d77de7a767f49350
1m-lcei <187761927+1m-lcei@users.noreply.github.com>
2026-05-03 14:56:45 +0900

    refactor: Separate SVG icons and theme controller from App.tsx

--- src/components/SvgIcons.tsx
+++ src/components/SvgIcons.tsx
@@ -11,3 +11,22 @@ export function BaselineArrowForwardIos(props: SVGProps<SVGSVGElement>) {
     </svg>
   );
 }
+
+export function ErrorIcon(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <title>エラーアイコン</title>
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+      />
+    </svg>
+  );
+}
