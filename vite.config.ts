6bf494ec397c7408c528518a1254226751a602c6
1m-lcei <187761927+1m-lcei@users.noreply.github.com>
2026-09-22 07:11:35 +0900

    build: add vite plugin to delete html comments

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,15 @@ import { defineConfig } from "vite";
 
 export default defineConfig(({ command }) => ({
   base: command === "build" ? "/kuto-ladder/" : "/",
+  plugins: [
+    {
+      name: "strip-html-comments",
+      apply: "build",
+      transformIndexHtml(html) {
+        return html
+          .replace(/<!--[\s\S]*?-->/g, "")
+          .replace(/^[ \t]*\r?\n/gm, "");
+      },
+    },
+  ],
 }));
